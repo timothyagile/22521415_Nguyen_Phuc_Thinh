29d39287f8b6a777956d7caad6e6e221422f9f12
Agile <22521415@gm.uit.edu.vn>
2022-10-26 17:58:00 +0700

    ud
    bai71

--- Bai71/71.cpp
+++ Bai71/71.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
+void Nhap(float&, int&);
+float Tong(float, int);
 int main()
 {
 	float x;
+	int n;
+	Nhap(x, n);	
+	cout << "Gia tri cua S la: " << Tong(x, n);
+	return 0;
+}
+void Nhap(float& x, int& n)
+{
 	cout << "Nhap x: ";
 	cin >> x;
-
-	int n;
 	cout << "Nhap n: ";
 	cin >> n;
-
-	int s = 0;
+}
+float Tong(float x, int n)
+{
+	float s = 0;
 	int t = x;
 	int i = 1;
-	while (i <= 2*n + 1)
+	while (i <= 2 * n + 1)
 	{
 		s = s + t;
 		t = t * x * x;
 		i = i + 2;
 	}
-	cout << "Gia tri cua S la: " << s;
-	return 0;
+	return s;
 }
